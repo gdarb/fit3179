05d9166c7bd41ee434825d27243d6e5bb733721d
Aidan Miles <aidan.miles@gmail.com>
2023-10-07 15:51:09 +1100

    Add line chart for monthly passengers

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import line from "@/vega/line.vega.json";
 import map from "@/vega/map.vega.json";
 import { VegaLite } from "react-vega";
 
@@ -7,6 +8,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <VegaLite spec={map} actions={false} />
+      {/* @ts-ignore */}
+      <VegaLite spec={line} actions={false} />
     </main>
   );
 }
