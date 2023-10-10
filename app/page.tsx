33b1ea821ed419dbb701f8d364c18bb6c7f852b0
Aidan Miles <aidan.miles@gmail.com>
2023-10-10 13:01:24 +1100

    Add boxplot for metro stations

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import box from "@/vega/box.vega.json";
 import line from "@/vega/line.vega.json";
 import map from "@/vega/map.vega.json";
 import { VegaLite } from "react-vega";
@@ -10,6 +11,8 @@ export default function Home() {
       <VegaLite spec={map} actions={false} />
       {/* @ts-ignore */}
       <VegaLite spec={line} actions={false} />
+      {/* @ts-ignore */}
+      <VegaLite spec={box} actions={false} />
     </main>
   );
 }
