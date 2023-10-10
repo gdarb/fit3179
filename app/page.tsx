6396fb8729630ed4e89bb034856e63e85a46cfed
Aidan Miles <aidan.miles@gmail.com>
2023-10-10 18:35:17 +1100

    Add panels to all visualisations

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-// @ts-nocheck
 "use client";
 
 import box from "@/vega/box.vega.json";
 import line from "@/vega/line.vega.json";
 import map from "@/vega/map.vega.json";
+import { ReactNode } from "react";
 import { VegaLite } from "react-vega";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between gap-y-5 px-24 py-12">
+    <main className="flex min-h-screen flex-col items-center justify-between gap-y-5 bg-gray-50 px-24 py-12">
       <div className="mx-auto max-w-3xl text-center">
         <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
           Transportation across Victoria
@@ -19,18 +19,32 @@ export default function Home() {
           aliqua.
         </p>
       </div>
-      <div>
+      <Panel>
         <VegaLite spec={map} actions={false} />
-      </div>
-      <div className="h-1 w-full bg-gray-500/50" />
-      <div>
+      </Panel>
+      <Panel>
+        {/* @ts-ignore */}
         <VegaLite spec={line} actions={false} />
-      </div>
-      <div className="h-1 w-full bg-gray-500/50" />
-      <div className="flex items-center gap-10">
-        <span>hello there</span>
-        <VegaLite spec={box} actions={false} />
-      </div>
+      </Panel>
+      <Panel>
+        <div className="flex items-center gap-10">
+          <span className="max-w-xs text-gray-700">
+            Pellentesque sagittis, nisi in ultrices rhoncus, odio metus tempus
+            lacus, vel facilisis turpis velit ac elit. Quisque efficitur velit
+            id eros sollicitudin, sit amet pulvinar diam pulvinar.
+          </span>
+          {/* @ts-ignore */}
+          <VegaLite spec={box} actions={false} />
+        </div>
+      </Panel>
     </main>
+  );
+}
+
+function Panel({ children }: { children: ReactNode }) {
+  return (
+    <div className="overflow-hidden rounded-lg bg-white shadow">
+      <div className="px-4 py-5 sm:p-6">{children}</div>
+    </div>
   );
 }
