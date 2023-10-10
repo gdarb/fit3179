585207c3f18a66843bf38f0c35189d96485dcd16
Aidan Miles <aidan.miles@gmail.com>
2023-10-10 18:28:27 +1100

    Add better spacing across the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 "use client";
 
 import box from "@/vega/box.vega.json";
@@ -7,12 +8,29 @@ import { VegaLite } from "react-vega";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between gap-y-5 p-24">
-      <VegaLite spec={map} actions={false} />
-      {/* @ts-ignore */}
-      <VegaLite spec={line} actions={false} />
-      {/* @ts-ignore */}
-      <VegaLite spec={box} actions={false} />
+    <main className="flex min-h-screen flex-col items-center justify-between gap-y-5 px-24 py-12">
+      <div className="mx-auto max-w-3xl text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          Transportation across Victoria
+        </h1>
+        <p className="mt-6 text-lg leading-8 text-gray-600">
+          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
+          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
+          aliqua.
+        </p>
+      </div>
+      <div>
+        <VegaLite spec={map} actions={false} />
+      </div>
+      <div className="h-1 w-full bg-gray-500/50" />
+      <div>
+        <VegaLite spec={line} actions={false} />
+      </div>
+      <div className="h-1 w-full bg-gray-500/50" />
+      <div className="flex items-center gap-10">
+        <span>hello there</span>
+        <VegaLite spec={box} actions={false} />
+      </div>
     </main>
   );
 }
