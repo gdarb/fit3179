b8fa2b0fcf0259a9a25cd09a1078874502dab919
Aidan Miles <aidan.miles@gmail.com>
2023-10-15 14:01:45 +1100

    Update document title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "FIT3179",
+  title: "FIT3179 / Data Visualisation 2",
 };
 
 export default function RootLayout({
